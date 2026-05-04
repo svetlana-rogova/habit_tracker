0e4ddd2df033c80238a3c1a79a5a9ae330293b0e
Светлана Рогова <mifistofiya@gmail.com>
2026-05-05 00:11:05 +0300

    Add model Habit

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+from tracker.validators import validate_habit_reward, validate_time_to_complete, validate_related_habit, \
+    validate_pleasant_habit, validate_periodicity
+
+
+class Habit(models.Model):
+    """
+    Модель привычки
+    """
+    owner = models.ForeignKey('user.CustomUser', on_delete=models.CASCADE, related_name='habit',
+                             verbose_name='Пользователь')
+    place = models.CharField(max_length=100,  verbose_name='Место')
+    time = models.DateTimeField(verbose_name='Время начала выполнения')
+    action = models.TextField(verbose_name='Действие')
+    pleasant_habit = models.BooleanField(default=False, verbose_name='Признак приятной привычки')
+    related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
+                                      verbose_name='Связанная привычка')
+    periodicity = models.PositiveIntegerField(default=1, verbose_name='Периодичность')
+    award = models.TextField(verbose_name='Вознаграждение')
+    time_to_complete = models.TimeField(verbose_name='Время на выполнение')
+    is_public = models.BooleanField(default=False, verbose_name="Опубликовать")
+
+    def __str__(self):
+        return f'{self.action}'
+
+    class Meta:
+        verbose_name = 'привычка'
+        verbose_name_plural = 'привычки'
