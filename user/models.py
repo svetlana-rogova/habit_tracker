19b7425621470fedde5533387b46d34b0aaf86a8
Светлана Рогова <mifistofiya@gmail.com>
2026-05-07 22:13:29 +0300

    Add authorization and registration

--- user/models.py
+++ user/models.py
@@ -5,8 +5,13 @@ class CustomUser(AbstractUser):
     """
     Модель пользователя
     """
-    chat_id = models.IntegerField(unique=True)
+    email = models.EmailField(unique=True, verbose_name='Email')
+    username = models.CharField(max_length=50, blank=True, null=True, verbose_name='Ник')
+    phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name='Номер телефона')
+    chat_id = models.CharField(max_length=50, blank=True, null=True, verbose_name='id чата в Telegram')
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
-        return self.chat_id
+        return self.email
--- user/views.py
+++ user/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from user.models import CustomUser
+from user.serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+
+
+class RegisterView(CreateAPIView):
+    """
+        Представление для регистрации.
+    """
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
