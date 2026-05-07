5fd76e0add5539b771b29bf270328b405cf988d0
Светлана Рогова <mifistofiya@gmail.com>
2026-05-07 22:16:05 +0300

    Add view for admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from tracker.models import Habit
+
+
+@admin.register(Habit)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'place', 'time', 'action', 'pleasant_habit', 'related_habit', 'periodicity', 'award',
+                    'time_to_complete', 'is_public')
+    search_fields = ('owner',)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.models import CustomUser
+
+
+@admin.register(CustomUser)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'phone_number', 'chat_id')
+    search_fields = ('email',)
+
